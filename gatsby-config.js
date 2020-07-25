fa17c78b8846e78ef9eb6dc4fdc1fdf5330c4c7f
SherlockSec <dan@sherlock-security.com>
2020-07-25 12:27:21 +0100

    Added RSS, may need more configuring, changed site URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,69 @@ module.exports = {
   siteMetadata: {
     title: `TryHackMe's Recipe Blog`,
     author: {
-      name: `TryHackMe`,
+      name: `TryHackMe Community`,
       summary: `TryHackMe goes brrrr`,
     },
     description: `The official recipe blog for TryHackMe`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    siteUrl: `https://tryrecipe.me`,
     social: {
       twitter: `realtryhackme`,
     },
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Your Site's RSS Feed",
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
